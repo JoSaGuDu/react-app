a84b66c7bb51c2153f9a0d2d7421579b80456bfc
JoSaGuDu <joseguti5@gmail.com>
2018-08-09 15:17:50 -0400

    undestanding components and elements

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+//Allow component refresh without refresihg the browser.
+if (module.hot) {
+  module.hot.accept();
+}
+
 registerServiceWorker();
